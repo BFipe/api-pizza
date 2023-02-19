1087b29d0963a1f5a0644210b619676f25ee3553
BFipe <ilya.maximov11@gmail.com>
2023-02-19 13:27:34 +0300

    alpha 0.0.5 bson pizza id

--- api-pizza/Entities/Pizza.cs
+++ api-pizza/Entities/Pizza.cs
@@ -1,7 +1,12 @@
-﻿namespace api_pizza.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace api_pizza.Entities
 {
     public class Pizza
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Title { get; set; }
         public string Img { get; set; }
